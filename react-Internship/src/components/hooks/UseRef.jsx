9fdada4c88792b275470a2b596ccf7e597846721
srushtigajera11 <srushtigajera113@gmail.com>
2026-01-27 11:57:11 +0530

    useRef task completed

--- react-Internship/src/components/hooks/UseRef.jsx
+++ react-Internship/src/components/hooks/UseRef.jsx
@@ -1,26 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useRef } from 'react'
 export default function Refeg() {
     let nameref = useRef()
     let resultref = useRef()
+    let ageRef = useRef()
+    const [age,setage] = useState(0);
+    const [color,setcolor] = useState('blue');
     let showmsg=()=>
     {
-        let msg = "welcome "+ nameref.current.value
-        resultref.current.innerHTML = msg
-        nameref.current.value=""
+        
+        if(ageRef.current.value >= 18){
+            let msg = "eligibel to vote "+ nameref.current.value
+            resultref.current.innerHTML = msg
+            setcolor('green');
+              nameref.current.value=""
+              ageRef.current.value=''
+            return;
+        }else{
+            let msg = "not eligible to vote "+ nameref.current.value
+            resultref.current.innerHTML = msg
+            setcolor('red');
+              nameref.current.value=""
+              ageRef.current.value=''
+        }
+     
+        
+
     }
   return (
-    <div>Refeg:
-        <p>
-            Enter name : <input type="text"className='p-3 border' ref={nameref} />
+    <div className=' pt-5 bg-blue-400 '>Refeg:
+        <p className='h-10 '>
+            Enter name : <input type="text"className='p-1 border' ref={nameref} />
             
         </p>
-        <p >
+        <p  className='h-20'>
             age:
-            <input type="text"  className='p-3 border'/>
+            <input type="text"  className='p-1 border' ref={ageRef} />
         </p>
-        <input type="button" value="click here" onClick={()=>showmsg()} />
-        <h1 ref={resultref}>
+        <input type="button"  className=" border bg-amber-300 rounded text-center m-9 p-2"value="click here" onClick={()=>showmsg()} />
+        <h1 ref={resultref} style={{background:color}}>
 
         </h1>
     </div>
