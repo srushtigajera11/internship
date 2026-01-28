6c26da64c3efea4d2c321f02b848b2028c775beb
srushtigajera11 <srushtigajera113@gmail.com>
2026-01-28 22:32:26 +0530

    task done

--- react-Internship/src/components/e-com/ProductDetails.jsx
+++ react-Internship/src/components/e-com/ProductDetails.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { productsary } from './productData'
 export default function ProductDetails() {
   let params = useParams()
@@ -8,20 +8,20 @@ export default function ProductDetails() {
   if (!product) {
     return <div className="text-center mt-10">Product not found</div>
   }
-  let similar_products = productsary
-  // let similer_products = category_products.items.filter((i)=>i.id != pid)
-  // console.log(JSON.stringify(similer_products))
-  // let ui = similer_products.map((s)=>{
-  //   return  <div className='bg-amber-800 text-amber-50 text-2xl '>
-  //       <img src={s.imgurl} alt="" />
-  //       {s.name}
-  //       {s.price}
-  //   </div>
-  // })
-  
+  let similar_products = productsary.filter((i)=>i.category == product.category && i.id != pid)
+  console.log(similar_products)
+   let ui = similar_products.map((s)=>{
+      return <div key= {s.id} className='flex justify-center items-center text-center flex-col mt-7'>
+        <Link to={'/productDetails/'+s.id}>
+        <img src={s.thumbnail} className='h-1/3 ' alt="" />
+        <div className=' text-slate-900 text-base font-semibold line-clamp-2'>{s.title}</div>
+        <div>{s.price}</div>
+        </Link>
+      </div>
+    })
 
     return (
-    <div className="bg-slate-50 min-h-screen p-6">
+    <div className="bg-slate-50 min-h-screen">
       <div className="max-w-4xl mx-auto bg-white rounded-xl shadow-sm p-6 grid md:grid-cols-2 gap-8">
         
         {/* Image */}
@@ -48,6 +48,12 @@ export default function ProductDetails() {
           <button className="bg-sky-600 hover:bg-sky-700 text-white px-6 py-3 rounded-lg">
             Add to Cart
           </button>
+        </div>
+      </div>
+      <div className='mx-28'>
+        <p className='text-center text-3xl m-5  font-extrabold font-serif text-slate-900 '>Similar Products You may Like!</p>
+        <div className='flex gap-6 overflow-x-auto'>
+        {ui}
         </div>
       </div>
     </div>
